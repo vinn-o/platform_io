4faf9f52f32551163ba4cd6c870857602733dd5f
vinn-o <218386044+vinn-o@users.noreply.github.com>
2026-09-09 16:00:43 +0300

    task for blinking led

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,13 @@
 #include <Arduino.h>
 
+int led1 = 2; //pin where buildin LED is connected to
+
+void LedTask( *parameter){
+  ledstate = LOW;
+  while(true){
+    ledstate = !ledstate;                        //task for led
+    digitalWrite(led1, ledstate);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
